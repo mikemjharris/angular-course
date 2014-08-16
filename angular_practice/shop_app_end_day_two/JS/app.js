a85240a0c002a6e09d63c6f3f1aa97b1ad119c32
mikemjharris <mike.mj.harris@gmail.com>
2014-08-16 13:05:04 +0100

    setting up for day two practice

---
+++ angular_practice/shop_app_end_day_two/JS/app.js
@@ -0,0 +1,52 @@
+(function() {  
+  var app = angular.module('leatherLaneMarketApp', ['ngAnimate']);  
+
+    app.controller('MarketController', function(){  
+        this.basket = 0;
+         this.stalls = stalls;  
+         // this.selectedStall = stalls[0];
+
+      this.selectStall = function(stall) {
+        this.selectedStall = stall;
+    }
+
+
+      this.addToBasket = function() {
+        this.basket ++
+    }
+
+
+
+  });  
+
+
+stalls = [  
+  {  
+    name: "Burrito",  
+    price: 5,  
+    description:  "Meat and vegetables in a delicious wrap",  
+    availiable: true  
+  },  
+  {  
+    name: "Pizza",  
+    price: 6.5,  
+    description:  "Cheese and meat and veg on some dough",  
+    availiable: true  
+  },
+  {
+    name: "Burger",
+    price: 8.5,
+    description: "Meat in a bun",
+    availiable: true
+  },
+  {
+    name: "Fallafel",
+    price: 4,
+    description: "Fallafel - that's all",
+    availiable: true
+  }
+
+]  
+
+})();
+
